b352768d75e317e29085d877c7470a53918732fd
zalisaneri <irenasilaz17@student.upi.edu>
2025-11-05 19:02:01 +0700

    menambahkan file

---
+++ penjualan.py
@@ -0,0 +1,24 @@
+ #Irena Silmi Azizah
+#2500869
+#1b
+print("=== Sistem Analisis Performa Jualan ===")
+pertanyaan = int(input("berapa banyak produk yang terjual hari ini: "))
+data = []
+for i in range (1, pertanyaan +1):
+    if pertanyaan < 0 :
+        print("x input tidak valid harga harus berupa angka")
+    else :
+        harga = int(input(f"masukkan produk ke {i}: "))
+        data.append((harga))
+def total(data):
+    menghitung_total = sum(data)
+    return menghitung_total
+def rata(data):
+    rata_rata = sum(data)/len(data)
+    return rata_rata
+print("=== Hasil Analisis ===")
+print(f"Total pendapatan Rp. {total(data)}")
+print(f"Rata-rata pendapatan per produk Rp. {rata(data)}")
+
+
+
